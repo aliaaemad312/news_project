8adb3b982ac0bc44c4cedce22d98bff0ed22b18f
aliaaemad312 <aliaa.emad312@gmail.com>
2025-08-03 02:11:29 +0300

    + Initialize project. + create home screen.

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/ui/home/home_screen.dart';
+import 'package:news_app/utils/app_routes.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: AppRoutes.homeRouteName,
+      routes: {
+        AppRoutes.homeRouteName:(context)=>HomeScreen(),
+
+      },
+    );
+  }
+}
